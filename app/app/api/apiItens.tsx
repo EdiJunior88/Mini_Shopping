d6ae87e00dddc82e8e94cff49e76abd6e113ee30
EdiJunior88 <er.moura88@yahoo.com.br>
2024-02-13 23:00:40 -0300

    - Criando a API para os produtos - Chamando a API para testes no componente page.tsx - Adicionando a biblioteca axios para chamar a API - Adicionando o domínio para as imagens da API no arquivo next.config.mjs

---
+++ app/app/api/apiItens.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiItens = async () => {
+  const response = await axios.get(
+    "https://run.mocky.io/v3/c181cb8f-8707-4423-9f75-1771c3fe4542/products"
+  );
+  return response.data.products;
+};
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import { apiItens } from "./api/apiItens";
+import Image from "next/image";
 
-const page = () => {
-  return <div>page</div>;
+export const Page = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const data = await apiItens();
+      setItems(data);
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>
+          <h1>{item.nameProduct}</h1>
+          <p>{item.price}</p>
+          <Image
+            src={item.urlImage}
+            width={500}
+            height={500}
+            alt={item.nameProduct}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default page;
+export default Page;
